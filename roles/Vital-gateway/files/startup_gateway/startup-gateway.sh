25c6584a03ddbe09e96f8dea0e6665e0b5b2b55d
Jash Lee <s905060@gmail.com>
2014-12-20 21:22:38 -0500

    FIX
    FIX

--- roles/Vital-gateway/files/startup_gateway/startup-gateway.sh
+++ roles/Vital-gateway/files/startup_gateway/startup-gateway.sh
@@ -9,6 +9,7 @@ cd /home/vlab_scp/vmnet_conf/vlab-natdhcp/
 
 # uncomment the below line to setup VLAN 12 to be used without internet access
 ./nat-start.sh 12
+./nat-start.sh 14
 
 # uncomment the below line to setup VLAN 14 to be used with internet access
 #./nat-start.sh 14 +internet
@@ -25,7 +26,7 @@ iptables -t nat -A POSTROUTING -s 192.168.35.0/24 -o eth0 -j MASQUERADE
 #brctl addif xenbr0 eth2
 
 # startup controller
-cd /home/vlab_scp/cntl/interim/ 
+cd /home/vlab_scp/cntl/interim/
 
 #screen -d -m php cntl_interim-new.php
 nohup php cntl_interim-new.php >> /var/log/controller_interim.log 2>&1 &
--- roles/Xen-node/files/bld-startup/create-vm-bonds.sh
+++ roles/Xen-node/files/bld-startup/create-vm-bonds.sh
@@ -7,7 +7,7 @@ function create_bond {
     vconfig add bond0 $vlan
     #openvswitch command JL
     #ovs-vsctl set port bond0 tag=$vlan
-    #JL	 
+    #JL
     ifconfig bond0.$vlan up
   done
 
@@ -27,20 +27,21 @@ function add_bridge_if {
 	brctl addbr $brdg
 	#openvswitch command JL
 	#ovs-vsctl add-br $brdg
-	#JL 
+	#JL
 	ifconfig $brdg up
 	brctl addif $brdg $bond
 	#openvswitch command JL
 	#ovs-vsctl add-port $brdg $bond
-	#JL 
+	#JL
 #  done
 }
 
 #create_bond 11 12 14 16 18 20 22 24 112
-create_bond 12
+create_bond 12 14
 
 #add_bridge_if Net-TEST	 bond0.11
 add_bridge_if Net-CS6823 bond0.12
+add_bridge_if Net-CS6373 bond0.14
 #add_bridge_if inet-nat14 bond0.14
 #add_bridge_if Net-CS6233 bond0.16
 #add_bridge_if Net-CS6963 bond0.22
